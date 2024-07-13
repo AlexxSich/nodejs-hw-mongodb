399f67f8f2209f5d50c17c39d47d68d4757f6a30
AlexxSich <alexxsich@gmail.com>
2024-07-13 17:47:03 -0300

    ready

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -7,7 +8,36 @@ export const setupServer = () => {
   const app = express();
 
   app.get('/', (req, res) => {
-    res.json('Hello world!');
+    res.json('Вітаємо вас на сторінці контактів');
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        staus: 404,
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}`,
+      data: contact,
+    });
   });
 
   app.use((req, res, next) => {
